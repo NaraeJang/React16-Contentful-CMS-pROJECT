ac10cabe21d825caf3d05c4f0be62bf6afaba287
NaraeJang <j.naaaarae@gmail.com>
2023-10-18 12:58:28 -0400

    fetch proejcts

--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -1,4 +1,5 @@
 import { createClient } from "contentful";
+import { useEffect, useState } from "react";
 
 const client = createClient({
   space: "189v7f0dmbk6",
@@ -6,6 +7,31 @@ const client = createClient({
   accessToken: "cOT8NEmQr90OP2DRs7HyrBwffTULGtmfkDdiLJEgLAU",
 });
 
-client
-  .getEntries({ content_type: "projects" })
-  .then((response) => console.log(response));
+export const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  const getData = async () => {
+    setLoading(true);
+    try {
+      const response = await client.getEntries({ content_type: "projects" });
+      const projects = response.items.map((item) => {
+        const { title, url, image } = item.fields;
+        const id = item.sys.id;
+        const img = image?.fields?.file?.url;
+        return { title, url, id, img };
+      });
+      setProjects(projects);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return { loading, projects };
+};
